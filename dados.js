c74a5ca03a13ec54aef2bf542a5781c4a82845c4
Mariana Barbosa <marimbbarbosa93@gmail.com>
2024-09-06 12:03:19 -0300

    primeiro commit

---
+++ dados.js
@@ -0,0 +1,57 @@
+let dados = [ 
+
+///Objeto para Daiki
+    {
+        titulo: 'Natalia "Daiki" Vilela',
+        nascimento: "Data de Nascimento: 04 de outubro de 2004",
+        funcao: "Função: Iniciadora",
+        agentes: "Principais agentes: Sova, Skye e Fade",
+        descricao: "Daiki, cujo nome verdadeiro é Natalia Vilela, é a IGL da Team Liquid Visa e está no time desde sua entrada no cenário inclusivo, no ano de 2022. Atua como iniciadora e é seis vezes campeã brasileira de Valorant pela Team Liquid.",
+        link: "https://www.vlr.gg/player/11232/daiki",
+        tags: "inciadora igl capitã capita daikola",
+    },
+
+///Objeto para Bstrdd
+{
+    titulo: 'Paula "bstrdd" Naguil',
+    nascimento: "Data de Nascimento: 06 de maio de 2000",
+    funcao: "Função: Duelista",
+    agentes: "Principais agentes: Jett e Raze",
+    descricao: "bstrdd (lê-se bastarda) tem esse nick por causa do seu filme preferido Bastardos Inglórios. Junto com Daiki, está na Team Liquid Visa desde o início e é uma das maiores duelistas do cenário inclusivo.",
+    link: "https://www.vlr.gg/player/4807/bstrdd",
+    tags: "bastardinha bastarda manita chilena",
+},
+
+///Objeto para Isaa
+{
+    titulo: 'Isabeli "isaa" Esser',
+    nascimento: "Data de Nascimento: 08 de outubro de 2003",
+    funcao: "Função: Flex",
+    agentes: "Principais agentes: Viper, Gekko e Sova",
+    descricao: "Isabeli entrou na Team Liquid Visa durante a reestruturação da Line-Up. Destacou-se no cenário como duelista, mas assumiu a função de flex da Cavalaria devido à sua alta capacidade de adaptação aos mais variados cenários",
+    link: "https://www.vlr.gg/player/14386/isaa",
+    tags: "isa isaa flex",
+},
+
+///Objeto para Joojina
+{
+    titulo: 'Letícia "joojina" Paiva',
+    nascimento: "Data de Nascimento: 21 de maio de 2001",
+    funcao: "Função: Controladora",
+    agentes: "Principais agentes: Omen, Astra e Brimstone",
+    descricao: "Joojina juntou-se à Team Liquid Visa durante a reestruturação da Line-Up, em 2022. É uma das melhores controladoras do cenário e conhecida por vencer diversos clutchs, mostrando que não sa abala diante de situações difíceis. Também é High Elo em Dota2.",
+    link: "https://www.vlr.gg/player/15297/joojina",
+    tags: "omen dota chaves",
+},
+
+///Objeto para Bizerra
+{
+    titulo: 'Vitoria "Bizerra" Vieira',
+    nascimento: "Data de Nascimento: 06 de janeiro de 2003",
+    funcao: "Função: Sentinela",
+    agentes: "Principais agentes: Cypher, Killjoy e Viper",
+    descricao: "Bizerra, cujo nome verdadeiro é Vitória, entrou para o time da Team Liquid Valorant em 2023, durante a reestruturação da line-up. Venceu os dois splits do Game Changers de 2023 e o primeiro de 2024.",
+    link: "https://www.vlr.gg/player/12143/bizerra/",
+    tags: "beh béh faz",
+}
+]
